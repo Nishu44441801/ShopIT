483336d1c082a229527d565b3ad0b4a464fc6c62
Nishu44441801 <nishantpedgulkar4444@gmail.com>
2025-03-15 14:02:58 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,46 @@
+import express from "express";
+const app = express();
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDatabase } from "./config/dbConnect.js";
+import errorMidelwares from "./middlewares/errors.js";
+
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down due to uncaught exception");
+  process.exit(1);
+});
+
+dotenv.config({ path: "backend/config/config.env" });
+
+//connnting to database
+connectDatabase();
+
+app.use(express.json());
+app.use(cookieParser());
+
+// importing all routes this is for my understanding
+
+import productRoutes from "./routes/products.js";
+import authRoutes from "./routes/auth.js";
+import orderRoutes from "./routes/order.js";
+
+app.use("/api/v1", productRoutes);
+app.use("/api/v1", authRoutes);
+app.use("/api/v1", orderRoutes);
+
+app.use(errorMidelwares);
+
+const server = app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR: ${err}`);
+  console.log("Shutting down server due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
+});
---
+++ backend/controllers/orderControllers.js
@@ -0,0 +1,114 @@
+import { ExplainVerbosity } from "mongodb";
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
+import Product from "../models/product.js";
+import Order from "../models/order.js";
+import ErrorHandler from "../utils/errorHandler.js";
+
+export const newOrder = catchAsyncErrors(async (req, res, next) => {
+  const {
+    orderItems,
+    shippingInfo,
+    itemsPrice,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+    paymentMethod,
+    paymentInfo,
+  } = req.body;
+
+  if (!req.user) {
+    return next(new ErrorHandler("User not authenticated", 401));
+  }
+
+  const order = await Order.create({
+    orderItems,
+    shippingInfo,
+    itemsPrice,
+    taxAmount,
+    shippingAmount,
+    totalAmount,
+    paymentMethod,
+    paymentInfo,
+    user: req.user._id,
+  });
+
+  res.status(201).json({
+    success: true,
+    order,
+  });
+});
+
+export const myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    orders,
+  });
+});
+
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("No order found with this ID", 404));
+  }
+
+  res.status(200).json({
+    order,
+  });
+});
+
+export const allOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  res.status(200).json({
+    orders,
+  });
+});
+
+export const updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("No order found with this ID", 404));
+  }
+
+  if (order?.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  order?.orderItems?.forEach(async (item) => {
+    const product = await Product.findById(item?.product?.toString());
+    if (!product) {
+      return next(new ErrorHandler("No Product found with this ID", 404));
+    }
+    product.stock = product.stock - item.quantity;
+    await product.save({ validateBeforeSave: false });
+  });
+
+  order.orderStatus = req.body.status;
+  order.deliveredAt = Date.now();
+
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+export const deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("No Order found with this ID", 404));
+  }
+
+  await order.deleteOne();
+
+  res.status(200).json({
+    success: true,
+  });
+});
